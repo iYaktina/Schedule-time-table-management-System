980f4ae70bbb20d28b9a5ea7d173ee41575952f7
RahmaWael00 <rahmawael2120@gmail.com>
2024-10-26 20:10:31 +0300

    Update menu.php
    Inserted first part of themenu.php

--- menu.php
+++ menu.php
@@ -1,1 +1,12 @@
+<?php
+session_start(); // Start the session to track user login status
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Navigation</title>
+    <link rel="stylesheet" href="./css/menu.css"> <!-- Link to external CSS file -->
+</head>
