b451cf3ed41fceda7abc98963f478a070567e5a3
Mariam Attia <168276562+MariamAttia1@users.noreply.github.com>
2024-10-27 14:08:10 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -26,3 +26,37 @@
         }
     </script>
 </head>
+<body>
+    <div class="profile-container">
+    <div class="header">
+        <h2>Admin Panel</h2>
+        <img src="./images/22.png" alt="Profile Icon" class="profile-icon">
+    </div>
+
+    <div class="button-container">
+        <button onclick="showForm('addForm')">Add User</button>
+        <button onclick="showForm('deleteForm')">Delete User</button>
+        <button onclick="showForm('userList')">Get Users</button>
+    </div>
+
+    <div id="addForm" class="form-container">
+        <h3>Add User</h3>
+        <form action="add_user.php" method="POST">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter Username" required>
+            
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter Password" required>
+
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter Email" required>
+            
+            <label for="usertype">Usertype</label>
+            <input type="text" id="usertype" name="usertype" placeholder="Enter Usertype" required>
+
+            <button type="submit">Add User</button>
+                  <button type="button" onclick="window.location.href='?';">Back</button>
+        </form>
+    </div>
+</body>
+</html>
