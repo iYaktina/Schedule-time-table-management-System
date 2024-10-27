06b9c2f69efa534df18224292f924980c0c1dba5
RahmaWael00 <rahmawael2120@gmail.com>
2024-10-27 04:08:42 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -10,3 +10,36 @@ session_start();
     <title>Website Navigation</title>
     <link rel="stylesheet" href="./css/menu.css"> 
 </head>
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="index.php">STMS</a> 
+        </div>
+        <ul class="nav-links">
+       <?php
+            if (isset($_SESSION['ID'])) {
+                if($_SESSION['Usertype']=='User'){
+                echo '
+                    <li><a href="profile.php">View Profile</a></li>
+                    <li><a href="schedule.php">View Schedule</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                ';
+                }
+                else   if($_SESSION['Usertype']=='Instructor'){
+                    echo '
+                        <li><a href="Instructor.php">Instructor Page</a></li>
+                        <li><a href="profile.php">View Profile</a></li>
+                        <li><a href="schedule.php">View Schedule</a></li>
+                        <li><a href="logout.php">Logout</a></li>
+                    ';
+                    }
+                else{
+                    echo '
+                    <li><a href="admin.php">Admin Page</a></li>
+                    <li><a href="profile.php">View Profile</a></li>
+                    <li><a href="schedule.php">View Schedule</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                ';
+
+                }
+            }
