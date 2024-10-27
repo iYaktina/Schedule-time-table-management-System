5712130fcff17b97fd4b6f663aaf621732b49665
RahmaWael00 <rahmawael2120@gmail.com>
2024-10-27 04:11:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schedule Management System</title>
+    <link rel="stylesheet" href="./css/home.css">
+</head>
+<body>
+            <?php include('menu.php'); ?>
+<div class="hero-section">
+    <div class="hero-content">
+        <h1>Schedule timetable</h1>
+        <p class="subtitle">Management System</p>
+        <a href="#about-us" class="scroll-btn">More Details</a>
+    </div>
+</div>
