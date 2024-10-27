39f3eb7d6aaef20cc5cb9e06b82672190fdad0b5
Mariam Attia <168276562+MariamAttia1@users.noreply.github.com>
2024-10-27 11:11:26 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,1 +1,28 @@
+<?php
+    include_once "include/dbh.inc.php";
+    include('menu.php');
 
+    if (!isset($_SESSION['ID'])) {
+        echo "<script>alert('Please Login!');</script>";
+        header("Refresh: 0;URL=login.php");
+    }
+    if($_SESSION['Usertype'] != 'Admin'){
+        echo "<script>alert('Classified for admins only');</script>";
+        header("Refresh: 0;URL=index.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="./css/admin.css">
+    <script>
+        function showForm(formId) {
+            document.querySelectorAll('.form-container').forEach(form => form.style.display = 'none');
+            document.getElementById(formId).style.display = 'block';
+        }
+    </script>
+</head>
