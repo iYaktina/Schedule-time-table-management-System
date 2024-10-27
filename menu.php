6550198ec22f2df249ffca1a84932c4501f592ac
iYaktina <168226966+iYaktina@users.noreply.github.com>
2024-10-27 04:09:24 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -43,3 +43,18 @@ session_start();
 
                 }
             }
+ else {
+                echo '
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                ';
+            }
+            ?>
+        </ul>
+    </nav>
+
+    
+</body>
+</html>
+
